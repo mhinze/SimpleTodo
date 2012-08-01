bacc4b65964e6f20f55079cea820db241e6c0d5c
John Crowe <john.byron.crowe@gmail.com>
2012-08-01 12:16:41 -0500

    Got appharbor working and updated with automatic migrations, this commit switches the project back to explicit migrations.

--- SimpleTodo/Migrations/Configuration.cs
+++ SimpleTodo/Migrations/Configuration.cs
@@ -1,13 +1,15 @@
 namespace SimpleTodo.Migrations
 {
+    using System;
+    using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<SimpleTodo.Models.Context>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
-            AutomaticMigrationDataLossAllowed = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(SimpleTodo.Models.Context context)
