fe9dc8fe3c595f503af57b12e61ed0e5baab3030
John Crowe <john.byron.crowe@gmail.com>
2012-08-01 12:32:45 -0500

    testing explicit migrations...

--- SimpleTodo/Migrations/Configuration.cs
+++ SimpleTodo/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace SimpleTodo.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(SimpleTodo.Models.Context context)
